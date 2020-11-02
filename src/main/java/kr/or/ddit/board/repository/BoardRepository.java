f6ec419295604f1dce0be11b1d078c56753aa8c3
dam <qkrekal1389@gmail.com>
2020-11-02 14:35:26 +0900

    spring 개요 spring 프로젝트 설정 spring  . IOC  . DI/DL
    실습
     . 스프링 컨테이너
     . bean 엘레멘트 속성

---
+++ src/main/java/kr/or/ddit/board/repository/BoardRepository.java
@@ -0,0 +1,23 @@
+package kr.or.ddit.board.repository;
+
+import kr.or.ddit.board.model.BoardVO;
+
+public class BoardRepository implements BoardRepositoryI{
+
+	private BoardRepositoryI boardRepository;
+
+	@Override
+	public BoardVO getBoard(int boardNo) {
+		// db에서 조회를 해야한, 지금은 설정이 갖춰지지 않았으므로
+		// 가짜 객체(Mock)를 반환한다
+		if(boardNo == 1) {
+			return new BoardVO(1, "첫번째 글", "내용");
+		}
+		else {
+			return null;
+		}
+		
+	}
+	
+	
+}
